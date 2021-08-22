744f6f7657c0e34b208f0b5d8a4eab28a59942db
Oleksii Zghurskyi <mr.noone@icloud.com>
2021-08-22 13:28:04 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "CoreKit",
+  products: [
+    .library(name: "CoreKit", targets: ["CoreKit"])
+  ],
+  dependencies: [],
+  targets: [
+    .target(name: "CoreKit", dependencies: [])
+  ]
+)
