b71ae51bcb5d0f9238c4dec9a3831d84d1f26e9c
Oleksii Zghurskyi <mr.noone@icloud.com>
2021-11-04 13:39:49 +0200

    Remove unnecessary extensions

--- Sources/CoreKit/Extensions/Date.swift
+++ Sources/CoreKit/Extensions/Date.swift
@@ -13,21 +13,3 @@ public extension Date {
     Calendar.current.component(.year, from: self)
   }
 }
-
-extension Date: LosslessStringConvertible {
-  var description: String {
-    let formatter = DateFormatter.iso8601
-    return formatter.string(from: self)
-  }
-  
-  public init?(_ description: String) {
-    let formatter = DateFormatter.iso8601
-    let date = formatter.date(from: description)
-    
-    if let date = date {
-      self = date
-    } else {
-      return nil
-    }
-  }
-}
