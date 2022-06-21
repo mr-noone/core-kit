2b22a338847dd171f7831aa209588aa5da8ac5ad
Oleksii Zghurskyi <mr.noone@icloud.com>
2022-06-21 14:57:56 +0300

    Add separate string extension

--- Sources/CoreKit/Extensions/String.swift
+++ Sources/CoreKit/Extensions/String.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 public extension String {
+  func separate(every: Int, with separator: String) -> String {
+    return String(stride(from: 0, to: Array(self).count, by: every).map {
+      Array(Array(self)[$0..<min($0 + every, Array(self).count)])
+    }.joined(separator: separator))
+  }
+  
   func substringMatches(regex: String) throws -> [String] {
     let regex = try NSRegularExpression(pattern: regex, options: [])
     let range = NSMakeRange(0, (self as NSString).length)
