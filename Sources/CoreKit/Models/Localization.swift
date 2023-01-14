45f5ef27586a75350d729291fe61534730904fcb
Oleksii Zghurskyi <mr.noone@icloud.com>
2023-01-14 12:50:48 +0200

    Add localized string with arguments

---
+++ Sources/CoreKit/Models/Localization.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public enum LS {
+    public enum Common: LocalizedTable {
+        @Localizable("LS.Common.warningTitle", Self.self, .module)    public static var warningTitle
+        @Localizable("LS.Common.errorTitle", Self.self, .module)      public static var errorTitle
+        @Localizable("LS.Common.unexpectedError", Self.self, .module) public static var unexpectedError
+        
+        @Localizable("LS.Common.ok", Self.self, .module)              public static var ok
+        @Localizable("LS.Common.cancel", Self.self, .module)          public static var cancel
+        @Localizable("LS.Common.done", Self.self, .module)            public static var done
+    }
+}
